faab9a8b600c48a1cf16400840a29d9f5bb26941
Bloctans <93960460+Bloctans@users.noreply.github.com>
2022-09-05 14:44:20 -0500

    2

--- src/screens/Groupchat.svelte
+++ src/screens/Groupchat.svelte
@@ -1,11 +1,14 @@
 <script>
 
     import Container from "../lib/Container.svelte"
+    import Post from "../lib/Post.svelte";
     import {chatid_stor,spinner} from "../lib/stores.js";
 
+    let id = 0;
     let chatlist = ""
     let inchat = false
     let chatid = ""
+    let chatname = ""
     let chat_res = ""
     let chat_res_res = false
     export let chats = [];
@@ -29,7 +32,35 @@
             }
             chats = chats;
         } else if (cmd.val.post_origin == chatid) {
-
+            chatposts.unshift({
+                id: id++,
+                post_id: cmd.val.post_id,
+                user: cmd.val.u,
+                content: cmd.val.p,
+                date: cmd.val.t.e,
+            })
+            chatposts = chatposts //svelte rendering sucks
+        } else if (cmd.val.chatid = chatid) {
+            const d = new Date();
+            let time = d.getTime()/1000;
+            if (cmd.val.state == 1) {
+                chatposts.unshift({
+                    id: id++,
+                    post_id: "system "+id,
+                    user: "System",
+                    content: cmd.val.u + " Has Joined " + chatname + "!",
+                    date: time,
+                })
+            } else if (cmd.val.state == 0) {
+                chatposts.unshift({
+                    id: id++,
+                    post_id: "system "+id,
+                    user: "System",
+                    content: cmd.val.u + " Has Left " + chatname + "!",
+                    date: time,
+                })
+            }
+            chatposts = chatposts //svelte rendering sucks
         }
     })
 
@@ -38,28 +69,54 @@
     async function loop_g() {
         if (inchat) {
             if (!chat_res_res) {
-                let headers = new Headers()
-                headers.append("username", localStorage.getItem("meower_savedusername"))
-                headers.append("token", localStorage.getItem("meower_savedpassword"))
-                const resp = await fetch(
-                    "https://api.meower.org/posts/"+chatid+"?autoget",
-                    {headers: headers}
-                );
-                console.log(await resp)
-                const json = await resp.json();
-                chat_res = json
-                chatid_stor.set(chatid)
-                cljs.send({
-                    cmd: "direct", 
-                    val: {
-                        cmd: "set_chat_state", 
+                if (chatid == "livechat") {
+                    chatid_stor.set(chatid)
+                    cljs.send({
+                        cmd: "direct", 
                         val: {
-                            chatid: chatid, 
-                            state: 1
+                            cmd: "set_chat_state", 
+                            val: {
+                                chatid: chatid, 
+                                state: 1
+                            }
                         }
+                    })
+                    chat_res_res = true
+                } else {
+                    let headers = new Headers()
+                    headers.append("username", localStorage.getItem("meower_savedusername"))
+                    headers.append("token", localStorage.getItem("meower_savedpassword"))
+                    const resp = await fetch(
+                        "https://api.meower.org/posts/"+chatid+"?autoget",
+                        {headers: headers}
+                    );
+                    console.log(await resp)
+                    const json = await resp.json();
+                    chat_res = json
+                    for (var i = 0; i < chat_res.autoget.length; i++) {
+                        var post = chat_res.autoget[i]
+                        chatposts.push({
+                            id: id++,
+                            post_id: post.post_id,
+                            user: post.u,
+                            content: post.p,
+                            date: post.t.e,
+                        })
                     }
-                })
-                chat_res_res = true
+                    chatposts = chatposts //svelte rendering sucks
+                    chatid_stor.set(chatid)
+                    cljs.send({
+                        cmd: "direct", 
+                        val: {
+                            cmd: "set_chat_state", 
+                            val: {
+                                chatid: chatid, 
+                                state: 1
+                            }
+                        }
+                    })
+                    chat_res_res = true
+                }
             }
         }
         window.requestAnimationFrame(loop_g)
@@ -68,7 +125,7 @@
 
 <div class="Groupchat">
     {#if inchat}
-        `<form 
+        <form 
             class="createpost"
             on:submit|preventDefault={e => {
                 const EmojiRegex = new RegExp(/[\u{1F346}]/ug)
@@ -119,12 +176,28 @@
             >
             <button>Post</button>
         </form>
+        <br />
+        <h1>{chatname}</h1>
         {#if chat_res.error}
             <p>{"Error While Loading, Error: " + chat_res.type}</p>
         {:else}
-            <p>{JSON.stringify(chat_res)}</p>
+            {#each chatposts as chatpost (chatpost.id)}
+                <div>
+                    <Post post={chatpost} />
+                </div>
+            {/each}
         {/if}
     {:else}
+        <button on:click = {()=>{
+            chatid = "livechat"
+            chatname = "Livechat"
+            inchat = true
+        }}
+        >
+            Join LiveChat
+        </button>
+        <br />
+        <br />
         {#each chats as chat (chat._id)}
             <div>
                 <Container>
@@ -133,11 +206,15 @@
                             class="circle join"
                             on:click = {()=>{
                                 chatid = chat._id
+                                chatname = chat.nickname
                                 inchat = true
                             }}
                         ></button>
                         <button
                             class="circle leave"
+                            on:click = {()=>{
+                                alert("Because of issues, you cant leave")
+                            }}
                         ></button>
                     </div>
 
