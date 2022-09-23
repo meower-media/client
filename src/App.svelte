1a3b409c9da2517e4518d33a1691750483beb6e7
M.D. Walters <808426@pdsb.net>
2022-09-22 21:02:08 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -169,16 +169,16 @@
 	}
 
 	::-webkit-scrollbar-track {
-  		background-color: #a15d04;
+  		background: #a15d04;
   		border-radius: 10px;
 	}
 
 	::-webkit-scrollbar-thumb {
-  		background-color: var(--orange); // #f9a636 
+  		background: #f9a636;
   		border-radius: 10px;
 	}
 
 	::-webkit-scrollbar-thumb:hover {
-  		background-color: var(--orange); // #f9a636 
+  		background: #f9a636;
 	}
 </style>
