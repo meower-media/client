9dca910ccb3b889c982833181100197bd9ea4493
JoshAtticus <72828296+JoshAtticus@users.noreply.github.com>
2022-10-29 18:15:27 +0800

    Add new article to news

--- src/screens/News.svelte
+++ src/screens/News.svelte
@@ -8,6 +8,13 @@
     Get the latest news about BetterMeower
 </Container>
 <Container>
+    <h2>v1.4.0 in development!</h2>
+    <p>29 October 2022 | JoshAtticus</p>
+    <hr>
+    <p>BetterMeower v1.4.0 is here! Although it's not quite what we'd call ready yet, you can test it and leave feedback on our GitHub!</p>
+    <p><b>You can leave some feedback on BetterMeower v1.4.0 <a href="https://github.com/BetterMeower-Project/BetterMeower/issues">here</a></b></p>
+</Container>
+<Container>
     <h2>v1.4.0 Status Update</h2>
     <p>10 October 2022 | JoshAtticus</p>
     <hr>
