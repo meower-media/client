932bfe15cbf1abec2465ffb04ee5929619c74812
M.D. Walters <808426@pdsb.net>
2022-11-03 13:52:44 -0400

    Actually implement news

--- src/screens/Main.svelte
+++ src/screens/Main.svelte
@@ -31,6 +31,8 @@
 			<Chatlist />
 		{:else if $page === "groupchat"}
 			<Groupchat />
+		{:else if $page === "news"}
+			<News />
 		{:else if $page === "profile"}
 			<Profile />
 		{:else if $page === "recent"}
