e79c7e0d34408d81bf3372f2bff6490fce56631f
M.D. Walters <808426@pdsb.net>
2022-11-03 13:54:57 -0400

    .

--- src/screens/News.svelte
+++ src/screens/News.svelte
@@ -5,7 +5,7 @@
 
 <Container>
     <h1>News</h1>
-    Get the latest news about BetterMeower
+    Get the latest news about BetterMeower.
 </Container>
 <Container>
     <h2>v1.4.0 in development!</h2>
