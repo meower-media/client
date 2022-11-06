4ad4c5f51430f45ae7a7b1df66f6e0e2b623e5b6
Bloctans <93960460+Bloctans@users.noreply.github.com>
2022-11-05 23:45:36 -0500

    Update Setup.svelte

--- src/screens/Setup.svelte
+++ src/screens/Setup.svelte
@@ -216,7 +216,7 @@
 				<p class="small">(Several features will be unavailable while not logged in.)</p>
 				<div>
 					<p class="small">
-						Meower Svelte v1.4.1
+						Meower Svelte v1.5_DEVBUILD
 					</p>
 					<img
 						src={meowy}
