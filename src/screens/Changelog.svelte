54024d7bf27626da3457926ba3dbb418f418988b
CST1229 <68464103+CST1229@users.noreply.github.com>
2022-12-29 18:41:32 +0100

    Update changelog

--- src/screens/Changelog.svelte
+++ src/screens/Changelog.svelte
@@ -7,6 +7,12 @@
 	What's new?
 </Container>
 <Container>
+	<h2>1.5.1</h2>
+	<ul>
+		<li>Fix the dango profile picture appearing twice when you have it selected</li>
+	</ul>
+</Container>
+<Container>
 	<h2>1.5</h2>
 	<ul>
 		<li>
