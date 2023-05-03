0a4425816b3201417279d368af22cb234a7709b8
M.D. Walters <popularvsofficial@gmail.com>
2023-05-03 11:28:15 -0400

    hmm

--- src/lib/twemoji-utils.js
+++ src/lib/twemoji-utils.js
@@ -1,7 +1,17 @@
 export function toHTMLElement(text) {
+	const text_split = text.split("");
+	
+	for (const i in text_split) {
+		if (text_split[i] == "<") {
+			text_split[i] = "&lt";
+		} else if (text_split[i] == ">") {
+			text_split[i] = "&gt";
+		}
+	}
+	
     const div = document.createElement("div");
     const p = document.createElement("p");
-    p.innerText = text;
+    p.innerText = text_split.join("");
     div.appendChild(p);
     return div;
 }
