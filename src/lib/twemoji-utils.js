2000304f1ae71096fdef9a30a41fd604311ab34f
M.D. Walters <popularvsofficial@gmail.com>
2023-05-14 20:17:12 -0400

    Update src/lib/twemoji-utils.js
    Co-authored-by: CST1229 <68464103+CST1229@users.noreply.github.com>

--- src/lib/twemoji-utils.js
+++ src/lib/twemoji-utils.js
@@ -1,19 +1,14 @@
 export function toHTMLElement(text) {
-	const text_split = text.split("");
-	
-	for (const i in text_split) {
-		if (text_split[i] == "<") {
-			text_split[i] = "&lt";
-		} else if (text_split[i] == ">") {
-			text_split[i] = "&gt";
-		} else if (text_split[i] == "\n") {
-			text_split[i] = "<br>";
-		}
-	}
-	
+    const filteredText = text
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;")
+        .replaceAll("'", "&apos;");
+    
     const span = document.createElement("span");
     const p = document.createElement("p");
-    p.innerText = text_split.join("");
+    p.innerText = filteredText;
     span.appendChild(p);
     return span;
 }
