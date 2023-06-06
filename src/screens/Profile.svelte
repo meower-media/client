9c9d6411fdb291cbf73fcf30eeeb6b66973fb524
WlodekM <75479945+WlodekM@users.noreply.github.com>
2023-06-06 11:21:30 +0300

    Update Profile.svelte

--- src/screens/Profile.svelte
+++ src/screens/Profile.svelte
@@ -23,7 +23,7 @@
 
 	const PFP_COUNT = 34;
 
-	const pfps = new Array(PFP_COUNT).fill().map((_, i) => i + 1);
+	const pfps = new Array(PFP_COUNT).fill().map((_, i) => i + 1).push(41)
 	let pfpSwitcher = false;
 
 	async function loadProfile() {
