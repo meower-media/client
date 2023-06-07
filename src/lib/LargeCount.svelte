d89cc3b165fafe85a62dc9ed590c8d8da64b29e5
CST1229 <68464103+CST1229@users.noreply.github.com>
2023-06-07 13:23:39 +0200

    Merge Dynamic count shortening in statistics (#151)

---
+++ src/lib/LargeCount.svelte
@@ -0,0 +1,21 @@
+<!-- A component for showing shortened large numbers (like 1k etc). -->
+<script>
+	export let num = 0;
+	// we don't need negative numbers but why not
+	$: magnitude = Math.abs(num);
+
+	function round(n, decimals) {
+		return +n.toFixed(decimals);
+	}
+</script>
+
+{#if (isNaN(num) || num === Infinity || num === -Infinity) || magnitude < 1000}
+	{num}
+{:else if magnitude < 1000000}
+	{round(num / 1000, 2)}k
+{:else if magnitude < 1000000000}
+	{round(num / 1000, 2)}M
+{:else}
+	<!-- how -->
+	{round(num / 1000, 2)}B
+{/if}
