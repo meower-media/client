08d5b6a0825cae59c737633c59edb9cff3747938
Tnix <tnix@meower.org>
2023-06-21 21:07:41 +1200

    Remove period within changelog for 1.6.1

--- src/screens/Changelog.svelte
+++ src/screens/Changelog.svelte
@@ -24,7 +24,7 @@
 	<h2>1.6.1</h2>
 	<ul>
 		<li>
-			Fixed a bug where the profile dropdown menu wasn't able to be opened on some iOS browsers and Safari for macOS.
+			Fixed a bug where the profile dropdown menu wasn't able to be opened on some iOS browsers and Safari for macOS
 		</li>
 		<li>
 			no select the kitty >:/
