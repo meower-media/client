0d00cf9f2744b13207e9540bb20290eccd97e59c
WlodekM <75479945+WlodekM@users.noreply.github.com>
2023-07-31 16:35:26 +0300

    Update keyDetect.js

--- src/lib/keyDetect.js
+++ src/lib/keyDetect.js
@@ -1,9 +1,21 @@
+import {modalShown, modalPage} from "./stores.js"
+var isKeyPressed = { 
+   'd': false, // Dev toools (Alt Shift D)
+   'i': false, // Was planned to be dev tools (Alt Shift I)
+   's': false, // Planned to be in a shortcut for settings
+}
 window.onkeydown = function (e) {
+	isKeyPressed[e.key] = true;
+	if (isKeyPressed["d"] && keydownEvent.altKey && keydownEvent.shiftKey) {
+		$modalShown = true
+		$modalPage = "devTools"
+	};
 	if (!e) e = window.event;
 	shiftHeld = e.shiftKey;
 };
 
 window.onkeyup = function (e) {
+	isKeyPressed[e.key] = false;
 	if (!e) e = window.event;
 	shiftHeld = e.shiftKey;
 };
