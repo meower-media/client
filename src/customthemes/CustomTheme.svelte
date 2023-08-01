e9df7b30d4dffead85e5c4aee416c17530775c1e
WlodekM <75479945+WlodekM@users.noreply.github.com>
2023-08-01 11:19:31 +0300

    My todo + Custom theme modal wip

---
+++ src/customthemes/CustomTheme.svelte
@@ -0,0 +1,170 @@
+<script>
+	import Modal from "../Modal.svelte";
+	import Loading from "../Loading.svelte";
+
+	import {modalShown, modalPage, authHeader, user} from "../stores.js";
+
+	import * as clm from "../clmanager.js";
+	import * as BGM from "../BGM.js";
+
+	let loading = false;
+	let loginStatus = "";
+	let username = "";
+	let password = "";
+	let rememberMe = false;
+
+	function doLogin() {
+		try {
+			loading = true;
+			clm.meowerRequest({
+				cmd: "direct",
+				val: {
+					cmd: "authpswd",
+					val: {
+						username: username,
+						pswd: password,
+					},
+				},
+			})
+				.then(async val => {
+					try {
+						const profileVal = await clm.meowerRequest({
+							cmd: "direct",
+							val: {
+								cmd: "get_profile",
+								val: val.payload.username,
+							},
+						});
+
+						modalShown.set(false);
+
+						user.update(v =>
+							Object.assign(v, {
+								...profileVal.payload,
+								name: val.payload.username,
+							})
+						);
+						authHeader.set({
+							username: val.payload.username,
+							token: val.payload.token,
+						});
+
+						if (rememberMe) {
+							localStorage.setItem(
+								"meower_savedusername",
+								username
+							);
+							localStorage.setItem(
+								"meower_savedpassword",
+								val.payload.token
+							);
+						}
+
+						BGM.playBGM($user.bgm_song);
+					} catch (e) {
+						loading = false;
+						loginStatus =
+							"Unexpected " + e + " error getting user data!";
+					}
+				})
+				.catch(code => {
+					loading = false;
+					switch (code) {
+						case "E:103 | ID not found":
+							loginStatus = "Invalid username!";
+							break;
+						case "I:011 | Invalid Password":
+							loginStatus = "Invalid password!";
+							break;
+						case "E:018 | Account Banned":
+							modalPage.set("banned");
+							break;
+						case "E:019 | Illegal characters detected":
+							loginStatus =
+								"Usernames must not have spaces or other special characters!";
+							break;
+						case "E:106 | Too many requests":
+							loginStatus =
+								"Too many requests! Please try again later.";
+							break;
+						default:
+							loginStatus = `Unexpected ${code} error!`;
+					}
+				});
+		} catch (e) {
+			console.log(e);
+			loading = false;
+			loginStatus = "Error logging in: " + e;
+		}
+	}
+</script>
+
+<Modal
+	on:close={() => {
+		$modalShown = false;
+	}}
+>
+	<h2 slot="header">Use custom theme</h2>
+	<div slot="default">
+		{#if loading}
+			<div class="fullcenter">
+				<Loading />
+			</div>
+		{:else}
+			<form
+				on:submit|preventDefault={e => {
+					if (!(e.target[0].value && e.target[1].value)) {
+						return false;
+					}
+					newTheme = e.target[0].value;
+					return false;
+				}}
+			>
+				<input
+					type="text"
+					class="modal-input white"
+					placeholder="Theme JSON"
+					maxlength="20"
+					value={theme}
+					autofocus
+				/><br />
+				<br />
+				<div class="modal-buttons">
+					<a
+						href="/"
+						on:click|preventDefault={() => {
+							modalPage.set("signup");
+						}}>Join Meower</a
+					>
+					<button type="submit">Login</button>
+				</div>
+			</form>
+		{/if}
+	</div>
+</Modal>
+
+<style>
+	.fullcenter {
+		text-align: center;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		top: 0;
+		left: 0;
+	}
+	.login-status {
+		color: red;
+		font-size: 105%;
+		display: inline-block;
+		height: 0;
+		overflow: visible;
+	}
+	label,
+	.checkboxes input {
+		vertical-align: middle;
+	}
+	.checkboxes {
+		text-align: left;
+		font-size: 95%;
+	}
+</style>
