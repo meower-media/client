4031f6f80e0de560e1447f1e9f4bfee03fa8dd98
WlodekM <75479945+WlodekM@users.noreply.github.com>
2023-08-01 17:24:20 +0300

    forgor: the prequel

--- src/lib/modals/Image.svelte
+++ src/lib/modals/Image.svelte
@@ -1,19 +1,23 @@
 <script>
-	import Modal from "../Modal.svelte";
+    import Modal from "../Modal.svelte";
 
-	import {modalShown, modalPage, imageClicked} from "../stores.js";
-    console.log($imageClicked.url)
+    import { modalShown, modalPage, imageClicked } from "../stores.js";
+    console.log($imageClicked.url);
 </script>
 
 <Modal
-	on:close={() => {
-		$modalShown = false;
-	}}
+    on:close={() => {
+        $modalShown = false;
+    }}
 >
-    <div slot="default">
+    <center>
         <img src={$imageClicked.url} title="image.png" />
+    </center>
+    <div slot="default">
         <center>
-            <a href={$imageClicked.url} target="black" style="max-width: 100%">Open the original image</a>
+            <a href={$imageClicked.url} target="black" style="max-width: 100%"
+                >Open the original image</a
+            >
         </center>
     </div>
 </Modal>
