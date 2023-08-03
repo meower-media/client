eb8efcd950a5dd31e28a7b03de41aa45c2e829ec
WlodekM <75479945+WlodekM@users.noreply.github.com>
2023-08-03 16:27:10 +0300

    debug

--- src/lib/keyDetect.js
+++ src/lib/keyDetect.js
@@ -2,11 +2,13 @@
 window.onkeydown = function (e) {
 	if (!e) e = window.event;
 	shiftHeld = e.shiftKey;
+	if (e.shiftKey) console.log("shift down")
 };
 
 window.onkeyup = function (e) {
 	if (!e) e = window.event;
 	shiftHeld = e.shiftKey;
+	if (e.shiftKey) console.log("shift up")
 };
 
 window.onmousemove = function (e) {
