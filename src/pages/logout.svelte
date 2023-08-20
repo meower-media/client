2095296afff1a38a30530daccb3a234e107b4b02
Tnix <tnix@meower.org>
2023-08-21 04:36:05 +1200

    let's see if this works

--- src/pages/logout.svelte
+++ src/pages/logout.svelte
@@ -6,14 +6,27 @@
 		user,
 		authHeader,
 	} from "../lib/stores.js";
-	import unloadedProfile from "../lib/unloadedProfile.js";
+	//import unloadedProfile from "../lib/unloadedProfile.js";
+	import {systemDarkMode} from "../lib/responsiveness.js";
 
 	import {goto} from "@roxi/routify";
 	import {onMount, tick} from "svelte";
 
 	onMount(async () => {
 		localStorage.clear();
-		user.set(unloadedProfile());
+		user.set({
+			name: null,
+			lvl: 0,
+			unread_inbox: false,
+			theme: "orange",
+			mode: !systemDarkMode(),
+			sfx: true,
+			bgm: false,
+			bgm_song: 2,
+			layout: "new",
+			pfp_data: 1,
+			quote: "",
+		});
 		authHeader.set({});
 		modalShown.set(false);
 		$goto("/");
