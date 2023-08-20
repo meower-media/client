7bb36d47c8222e5de931cd73ba3b7edab609ee51
Tnix <tnix@meower.org>
2023-08-21 04:42:43 +1200

    this isn't the best but I hope this at least works

--- src/lib/unloadedprofile.js
+++ src/lib/unloadedprofile.js
@@ -6,16 +6,14 @@
 
 import {systemDarkMode} from "./responsiveness.js";
 
-export default function unloadedProfile() {
-	return {
-		name: null,
-		theme: "orange",
-		mode: !systemDarkMode(),
-		sfx: true,
-		bgm: false,
-		bgm_song: 2,
-		layout: "new",
-		pfp_data: 1,
-		quote: "",
-	};
+export const unloadedProfile = {
+	name: null,
+	theme: "orange",
+	mode: !systemDarkMode(),
+	sfx: true,
+	bgm: false,
+	bgm_song: 2,
+	layout: "new",
+	pfp_data: 1,
+	quote: "",
 }
