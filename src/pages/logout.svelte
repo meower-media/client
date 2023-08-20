0fb24f49d29f010622c5eb5e07b19eae0df4285a
Tnix <tnix@meower.org>
2023-08-21 04:44:02 +1200

    undo last commit

--- src/pages/logout.svelte
+++ src/pages/logout.svelte
@@ -6,27 +6,14 @@
 		user,
 		authHeader,
 	} from "../lib/stores.js";
-	//import unloadedProfile from "../lib/unloadedProfile.js";
-	import {systemDarkMode} from "../lib/responsiveness.js";
+	import {unloadedProfile} from "../lib/unloadedProfile.js";
 
 	import {goto} from "@roxi/routify";
 	import {onMount, tick} from "svelte";
-
+	
 	onMount(async () => {
 		localStorage.clear();
-		user.set({
-			name: null,
-			lvl: 0,
-			unread_inbox: false,
-			theme: "orange",
-			mode: !systemDarkMode(),
-			sfx: true,
-			bgm: false,
-			bgm_song: 2,
-			layout: "new",
-			pfp_data: 1,
-			quote: "",
-		});
+		user.set(unloadedProfile);
 		authHeader.set({});
 		modalShown.set(false);
 		$goto("/");
