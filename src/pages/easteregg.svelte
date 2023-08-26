7c9e2d71a600327de48b53fe542aa95b39efd19b
Bloctans <93960460+Bloctans@users.noreply.github.com>
2023-08-25 22:36:18 -0500

    ok alot

--- src/pages/easteregg.svelte
+++ src/pages/easteregg.svelte
@@ -2,12 +2,30 @@
 <script>
 	import { modalPage, modalShown, OOBERunning, GroupCats} from "../lib/stores";
 	import Container from "../lib/Container.svelte";
+    import PFP from "../lib/PFP.svelte";
+    import LiText from "../lib/LiText.svelte";
 
+    import {levels} from "../lib/formatting.js"
 	import version from "../lib/version.js";
+
+    const PFP_COUNT = 34;
+
+	const pfps = new Array(PFP_COUNT).fill().map((_, i) => i + 1);
+	let pfpSwitcher = false;
+
+    let pfpOverflow = false;
+	$: {
+		const pfp = 1;
+		pfpOverflow = pfp < 0 || pfp > PFP_COUNT;
+	}
 
     let GCats = 100
 
-    let Profile_Username = "EasterEgg"
+    let Profile_Username = "EasterEgg";
+    let Profile_Level = 0;
+    let Profile_Quote = "Quote Here";
+    let Profile_PFP = 1;
+    let Profile_QuoteEnabled = true;
 </script>
 
 <div>
@@ -48,7 +66,7 @@
         >Update</button>
     </Container>
     <Container>
-        <!--Posts, Profiles, Profile Views, and PFP (Later on, maybe GC Member list and postlist)-->
+        <!--Posts, Profiles and Profile Views (Later on, maybe GC Member list and postlist)-->
         <h1>Make your own!</h1>
         Make your own profile, post, etc list with this!
 
@@ -62,31 +80,124 @@
             <input placeholder="Username" type="text" bind:value={Profile_Username} class="modal-input white"/>
             <br /><br />
 
-            Status
-
+            Quote
             <br />
-            <input placeholder="Username" type="text" bind:value={Profile_Username} class="modal-input white"/>
+            <input placeholder="Quote" type="text" bind:value={Profile_Quote} class="modal-input white"/>
+            <br /><br />
+
+            Enable Quote
+            <br />
+            <input type="checkbox" bind:value={Profile_QuoteEnabled} class="modal-input white"/>
             <br /><br />
 
             User Level
 
             <br />
-            <input placeholder="Username" type="text" bind:value={Profile_Username} class="modal-input white"/>
+            <select
+                class="grow"
+                on:change={e => {
+                    // @ts-ignore
+                    Profile_Level = e.target.value;
+                }}
+            >
+                <option value = 0>User</option>
+                <option value = 1>Low-level Moderator</option>
+                <option value = 2>Moderator</option>
+                <option value = 3>Administrator</option>
+                <option value = 4>Sysadmin</option>
+            </select>
             <br /><br />
 
             PFP
 
             <br />
-            <input placeholder="Username" type="text" bind:value={Profile_Username} class="modal-input white"/>
+            {#if pfpSwitcher}
+				<Container>
+					<h2>Profile Picture</h2>
+					<div id="pfp-list">
+						{#if pfpOverflow && Profile_PFP < 0}
+							<button
+								on:click={() => {
+									pfpSwitcher = false;
+								}}
+								class="pfp selected"
+								><PFP
+									online={false}
+									icon={Profile_PFP}
+									alt="Profile picture {Profile_PFP}"
+								/></button
+							>
+						{/if}
+						{#each pfps as pfp}
+							<button
+								on:click={() => {
+									Profile_PFP = pfp
+								}}
+								class="pfp"
+								class:selected={Profile_PFP === pfp}
+								><PFP
+									online={false}
+									icon={pfp}
+									alt="Profile picture {pfp}"
+								/></button
+							>
+						{/each}
+						{#if pfpOverflow && Profile_PFP > 0}
+							<button
+								class="pfp selected"
+								><PFP
+									online={false}
+									icon={Profile_PFP}
+									alt="Profile picture {Profile_PFP}"
+								/></button
+							>
+						{/if}
+					</div>
+                    <br /><br />
+                    <button
+                        class="long"
+                        title="Close"
+                        on:click={() => (pfpSwitcher = false)}
+                    >Close</button>
+				</Container>
+			{:else}
+				<button
+					class="long"
+					title="Change Profile Picture"
+					on:click={() => (pfpSwitcher = true)}
+					>Change Profile Picture</button
+				>
+			{/if}
             <br /><br />
-
-            <button
-                on:click={() => {
-
-                }}
-            >Update</button>
             
+            <Container>
+                <div class="profile-header">
+                    <PFP
+                        online={false}
+                        icon={Profile_PFP}
+                        alt="{Profile_Username}'s profile picture"
+                        size={1.4}
+                    />
+                    <div class="profile-header-info">
+                        <h1 class="profile-username">
+                            <LiText text={Profile_Username} />
+                        </h1>
+                        <div class="profile-active">
+                            Offline
+                        </div>
+                        <div class="profile-role">
+                            {levels[Profile_Level]}
+                        </div>
+                    </div>
+                </div>
+            </Container>
 
+            {#if Profile_QuoteEnabled}
+                <Container>
+                    <h3>Quote</h3>
+                    <p>"<i>{Profile_Quote}</i>"</p>
+                </Container>
+            {/if}
         </Container>
     </Container>
     <Container>
@@ -110,5 +221,60 @@
 </div>
 
 <style>
-	
+	.long {
+		width: 100%;
+		margin: 0;
+		margin-bottom: 0.2em;
+	}
+
+	.pfp {
+		padding: 0.2em;
+		margin: 0.2em;
+		border-radius: 1.45em;
+		display: inline-block;
+		background: none;
+		border: none;
+	}
+	:global(main.input-hover) .pfp:hover,
+	:global(main.input-touch) .pfp:active {
+		background-color: var(--orange-light);
+	}
+	/* cst todo: fix shadow appearing when activating then unhovering because i gtg*/
+	:global(:root main.input-hover) .pfp:active {
+		background-color: var(--orange-dark);
+	}
+	:global(main) .pfp.selected {
+		background-color: var(--orange);
+	}
+	#pfp-list {
+		display: flex;
+		flex-wrap: wrap;
+	}
+
+    .profile-header-info {
+		margin-left: 1em;
+		height: 6em;
+	}
+
+	.profile-active {
+		font-style: italic;
+	}
+
+	.profile-role {
+		position: absolute;
+		font-size: 90%;
+	}
+
+	.profile-username {
+		margin: 0;
+		display: inline-block;
+		max-width: 100%;
+		font-size: 3em;
+	}
+
+	.profile-header {
+		display: flex;
+		align-items: center;
+		flex-wrap: wrap;
+	}
 </style>
