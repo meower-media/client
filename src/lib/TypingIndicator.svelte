4d7b97ca3985b2dc65578e2aed8edc5adc161a4d
Bloctans <93960460+Bloctans@users.noreply.github.com>
2023-11-27 11:30:50 -0600

    ok

--- src/lib/TypingIndicator.svelte
+++ src/lib/TypingIndicator.svelte
@@ -22,7 +22,6 @@
 </script>
 
 {#if _typing.length > 0}
-<Container>
 	<span class="loading">
 		<span class="circle circle1" />
 		<span class="circle circle2" />
@@ -39,7 +38,6 @@
 			<span class="text">No one is typing, why can you see this</span>
 		{/if}
 	</span>
-</Container>
 {/if}
 
 <style>
