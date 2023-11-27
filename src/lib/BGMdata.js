cfcc5f2d3e94cd174944fd1421c32a6ba9ea09c0
Bloctans <93960460+Bloctans@users.noreply.github.com>
2023-11-27 14:38:50 -0600

    fonny

--- src/lib/BGMdata.js
+++ src/lib/BGMdata.js
@@ -7,54 +7,49 @@
 
 export const audioData = [
 	{
-		name: "Night sky",
-		author: "Meguy7878",
-		file: "0c38836da69714eae0f2a6662f9cbc58.wav",
-	},
-	{
-		name: "Percussion bass loop",
-		author: "Voxalice",
-		file: "6e1576462ac271ca5847a68dca722265.wav",
-	},
-	{
 		name: "Piano ear grapes",
 		author: "Maclator",
 		file: "2885eb62f10a5a18368d5481e1ac9f97.mp3",
 	},
 	{
-		name: "P.B loop remix",
-		author: "Johnelr",
-		file: "8fdc80585a238ea7a82adff23a274fb5.wav",
+		name: "Ballers",
+		author: "Michael",
+		file: "baller.mp3",
 	},
 	{
-		name: "Untitled Song 1",
-		author: "TheStrykerJay",
-		file: "c778052bfee4d6255c08411f60747bee.wav",
+		name: "OMG BLOOKA THEME",
+		author: "some guy named eric matyas",
+		file: "blockey.mp3",
 	},
 	{
-		name: "Meower.",
-		author: "Johnelr",
-		file: "e5bf32d41ea8c9dc1b2546fa7c7780bb.mp3",
+		name: "Epic poopoo",
+		author: "roblox",
+		file: "Epicshit.mp3",
 	},
 	{
-		name: "Untitled Song 2",
-		author: "Dotumantaraye",
-		file: "2c0293c58590d4d0ff3a0636f197d9ca.mp3",
+		name: "Xp theme",
+		author: "yayayaayayayayayayayay",
+		file: "Expericen.mp3",
 	},
 	{
-		name: "Meower is (not) dead",
-		author: "Voxalice",
-		file: "c25441be9d4f5547baf85cc444253259.mp3",
+		name: "I have personal info",
+		author: "yayayaayayayayayayayay",
+		file: "funny.mp3",
 	},
 	{
-		name: "The elevator of time",
-		author: "Voxalice",
-		file: "d56dc789ac58918b555c666935e56392.wav",
+		name: "parap",
+		author: "hair scar",
+		file: "HairScare.ogg",
 	},
 	{
-		name: "Meower is (not) dead Instrumental",
-		author: "Voxalice",
-		file: "8437988a9f23bb09fd6a50b280e5bed9.wav",
+		name: "some random song",
+		author: "i think from the rsause guy?",
+		file: "ORISIT.mp3",
+	},
+	{
+		name: "your ip",
+		author: "real",
+		file: "yourip.mp3",
 	},
 ];
 
