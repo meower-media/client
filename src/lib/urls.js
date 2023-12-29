f18338a4af1b3648e5cf4045bf7a1a2a218c145a
ShowierData9978 <mellfang36@gmail.com>
2023-12-29 00:45:05 -0600

    Change URL's

--- src/lib/urls.js
+++ src/lib/urls.js
@@ -3,10 +3,18 @@
  */
 
 export const beta = false;
+// get the current domain
+export const domain = window.location.host;
+export const isSecure = window.location.protocol === "https:";
 
 export const linkUrl =
-	localStorage.getItem("meower_linkurl") || "/api/v0/cloudlink"
+	localStorage.getItem("meower_linkurl") || (
+		window.location.hostname !== "localhost" ?
+			`ws${isSecure ? 's' : '' }://${domain}/api/v0/cloudlink`
+			: "ws://localhost:3001"
+		);
+		
 export const apiUrl =
-	localStorage.getItem("meower_apiurl") || "/api/"
+	localStorage.getItem("meower_apiurl") || `${window.location.protocol}//${domain}/api/`;
 export const encodeApiURLParams =
 	localStorage.getItem("meower_encodeapi") || false;
