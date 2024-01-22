b2ee5b293df32d63ed68c5d4f1c33c6304d60c58
Max Walters <popularvsofficial@gmail.com>
2024-01-22 18:15:10 +0000

    feat: start work on bottom sheet

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -1,7 +1,8 @@
 <script>
 	import {createEventDispatcher, onDestroy} from "svelte";
-	import {scale, fade} from "svelte/transition";
+	import {scale, fade, slide} from "svelte/transition";
 	import {expoOut} from "svelte/easing";
+    import {mobile} from "./responsiveness.js";
 
 	const dispatch = createEventDispatcher();
 	const close = () => dispatch("close");
@@ -52,23 +53,43 @@
 	transition:fade={{duration: 100, easing: expoOut}}
 />
 
-<div
-	class="modal"
-	role="dialog"
-	aria-modal="true"
-	bind:this={modal}
-	transition:scale={{start: 0.8, duration: 200, easing: expoOut}}
->
-	<div class="settings-controls">
-		{#if showClose}
-			<button class="circle close" title="Close modal" on:click={close} />
-		{/if}
-	</div>
+{#if $mobile}
+    <div
+	    class="bottom-sheet"
+	    role="dialog"
+	    aria-modal="true"
+	    bind:this={modal}
+	    transition:slide={{duration: 600, easing: expoOut}}
+    >
+        <div class="settings-controls">
+            {#if showClose}
+                <button class="circle close" title="Close modal" on:click={close} />
+            {/if}
+        </div>
 
-	<slot name="header" />
-	<hr />
-	<slot />
-</div>
+        <slot name="header" />
+        <hr />
+        <slot />
+    </div>
+{:else}
+    <div
+	    class="modal"
+	    role="dialog"
+	    aria-modal="true"
+	    bind:this={modal}
+        transition:scale={{start: 0.8, duration: 200, easing: expoOut}}
+    >
+        <div class="settings-controls">
+            {#if showClose}
+                <button class="circle close" title="Close modal" on:click={close} />
+            {/if}
+        </div>
+
+        <slot name="header" />
+        <hr />
+        <slot />
+    </div>
+{/if}
 
 <style>
 	.modal-background {
@@ -96,4 +117,19 @@
 		border: solid 2px var(--orange);
 		border-radius: 5px;
 	}
+
+    .bottom-sheet {
+        z-index: 9999;
+		position: fixed;
+		left: 50%;
+		bottom: -25%;
+		width: 90vw;
+		max-height: calc(100vh - 4em);
+		overflow: auto;
+		transform: translate(-50%, -50%);
+		padding: 1em;
+		background-color: var(--background);
+		border: solid 2px var(--orange);
+		border-radius: 30px 30px 0px 0px;
+    }
 </style>
