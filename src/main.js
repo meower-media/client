ea6e117b47edc05051b22c00658f89a45c258d37
Bloctans <93960460+Bloctans@users.noreply.github.com>
2024-03-23 01:11:51 -0500

    ok so i moved the init stuff

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import App from "./App.svelte";
-
 try {
 	if (navigator.serviceWorker) {
 		navigator.serviceWorker.getRegistration().then(registration => {
@@ -11,38 +9,3 @@ try {
 } catch (e) {
 	console.error(e);
 }
-
-function app() {
-	new App({
-		target: document.getElementById("app"),
-	});
-}
-
-var UA = navigator.userAgent.split(" ")
-// @ts-ignore
-UA = UA[UA.length-1]
-var UA2 = UA[UA.length-2]
-
-if (UA.includes("/")) {
-	// @ts-ignore
-	if ((UA.split("/")[0] == "Firefox" || UA.split("/")[0] == "OPR")) {
-		// @ts-ignore
-		if (parseInt(UA.split("/")[1]) >= 80) {
-			app()
-		} else {
-			document.body.innerHTML += '<div id="browser-support">	<img src="/src/assets/outdated.png" height="600px">	<a href="https://www.youtube.com/watch?v=Cz4aTSnFVvk" id="vivaldi" class="all-browsers"></a>	<a href="https://www.mozilla.org/firefox/" id="firefox" class="all-browsers small"></a>	<a href="https://www.google.com/chrome/" id="chrome" class="all-browsers small"></a>	<a href="https://www.opera.com/download" id="opera" class="all-browsers small"></a></div>'
-		}
-	}
-}
-
-if (UA2.includes("/")) {
-	if (UA2.split("/")[0] == "Chrome") { // Of course chrome needs the special treatment...
-		if (parseInt(UA2.split("/")[1]) >= 80) {
-			app()
-		} else {
-			document.body.innerHTML += '<div id="browser-support">	<img src="/src/assets/outdated.png" height="600px">	<a href="https://www.youtube.com/watch?v=Cz4aTSnFVvk" id="vivaldi" class="all-browsers"></a>	<a href="https://www.mozilla.org/firefox/" id="firefox" class="all-browsers small"></a>	<a href="https://www.google.com/chrome/" id="chrome" class="all-browsers small"></a>	<a href="https://www.opera.com/download" id="opera" class="all-browsers small"></a></div>'
-		}
-	}
-}			
-
-export default app;
